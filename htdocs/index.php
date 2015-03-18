8e48df1aeb981ab3cd126a9b602add0e64792311
Andrew Milsted <a.j.milsted@soton.ac.uk>
2015-03-18 11:27:54 +0000

    added accept header 303 sense

--- htdocs/index.php
+++ htdocs/index.php
@@ -23,7 +23,7 @@ $f3->set('UI','ui/');
 $f3->route('GET /',
 
 	function() {
-                $f3=Base::instance();
+		$f3=Base::instance();
 
 		$f3->set('title', 'homepage.title' );
 		$f3->set('content','homepage.html');
@@ -32,7 +32,7 @@ $f3->route('GET /',
 );
 $f3->route('GET /@path' , 
 	function() use($f3) {
-	        $path = $f3->get('PARAMS.path');
+		$path = $f3->get('PARAMS.path');
 		if( preg_match( "/\.html$/", $path ) )
 		{
 			$path = preg_replace( '/\.html$/','',$path );
@@ -43,7 +43,15 @@ $f3->route('GET /@path' ,
 		}
 		elseif( preg_match( "/^(code|scheme)-[A-Z0-9]+$/", $path ) )
 		{
-			header( "Location: http://cpv.data.ac.uk/turtle/$path.ttl" );
+			$wants = wants($f3); // do they like ttl or html?
+			if( $wants == "ttl" )
+			{
+				header( "Location: http://cpv.data.ac.uk/turtle/$path.ttl" );
+			}
+			else	
+			{
+				header( "Location: http://cpv.data.ac.uk/$path.html" );
+			}
 		}
 		else
 		{
@@ -59,3 +67,53 @@ $f3->route('GET /ns/*',
 $f3->run();
 
 exit;
+
+function wants($f3)
+{
+	$req = $f3->get('SERVER');
+
+	$views = array(
+		array(
+			"mimetypes"=>array( "text/html" ),
+			"ext"=>"html" ),
+		array(
+			"mimetypes"=>array( "text/turtle", "application/x-turtle" ),
+			"ext"=>"ttl" ),
+	);
+       
+	$ext = "html";
+	if( isset( $req["HTTP_ACCEPT"] ) )
+	{      
+		$opts = preg_split( "/,/", $req["HTTP_ACCEPT"] );
+		$o = array( "text/html"=>0.1 , "text/turtle"=>0 );
+		foreach( $opts as $opt)
+		{      
+			$opt = trim( $opt );
+			$optparts = preg_split( "/;/", $opt );
+			$mime = array_shift( $optparts );
+			$o[$mime] = 1;
+			foreach( $optparts as $optpart )
+			{      
+				$optpart = trim( $optpart );
+				list( $k,$v ) = preg_split( "/=/", $optpart );
+				$k = trim( $k );
+				$v = trim( $v );
+				if( $k == "q" ) { $o[$mime] = $v; }
+			}
+		}
+	       
+		$score = 0.1;
+		foreach( $views as $view )
+		{      
+			foreach( $view['mimetypes'] as $mimetype )
+			{      
+				if( @$o[$mimetype] > $score )
+				{      
+					$score=$o[$mimetype];
+					$ext = $view["ext"];
+				}
+			}
+		}
+	}
+	return $ext;
+}
